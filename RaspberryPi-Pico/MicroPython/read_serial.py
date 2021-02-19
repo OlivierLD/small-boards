e00c5ba02c4dfb020ad637745a3ac0f379232954
olivier.lediouris <olivier.lediouris@gmail.com>
2021-02-19 12:53:22 -0800

    Read GPS from Pico

--- RaspberryPi-Pico/MicroPython/read_serial.py
+++ RaspberryPi-Pico/MicroPython/read_serial.py
@@ -9,15 +9,30 @@
 #
 from machine import UART, Pin
 
-TX_PIN=16   # Pin(16) = GP16, pin #21. Green wire
+TX_PIN=16   # Pin(16) = GP16, pin #21. Green wire - Not required
 RX_PIN=17   # Pin(17) = GP17, pin #22. White wire
 BAUD_RATE=9600
 
+log_file = open("gps_log.nmea", "w")
+
 uart = UART(0, baudrate=BAUD_RATE, tx=Pin(TX_PIN), rx=Pin(RX_PIN), bits=8, parity=None, stop=1)
 
-while True:
-	# print("Any: {}".format(uart.any()))
-    if uart.any():
-          rcvChar = uart.read(1)  # 1 byte
-          print(rcvChar.decode("ascii"), end="")
+keep_looping = True
+while keep_looping:
+	try:
+		# print("Any: {}".format(uart.any()))
+	    if uart.any():
+	          rcvChar = uart.read(1)  # 1 byte
+	          nmea_string = rcvChar.decode("ascii")
+	          print(nmea_string, end="")
+	          log_file.write(nmea_string)
+	          log_file.flush()
+	except KeyboardInterrupt:
+		keep_looping = False
+		break     # Theorically useless
+	except Exception as ex:
+		print("Oops {}".format(ex))
 
+
+uart.close()
+log_file.close()
