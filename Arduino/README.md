7b445065bf9f0e10789ec99c72ce0cbd3437b7cb
olivier.lediouris <olivier.lediouris@gmail.com>
2021-02-20 07:58:33 -0800

    Doc for the Arduino and GPS

--- Arduino/README.md
+++ Arduino/README.md
@@ -8,5 +8,27 @@
 ### Wiring
 ![Wiring](./SerialDump/Arduino.GPS_bb.png)
 
+Here the GPS is an [Adafruit Ultimate GPS Breakout - 66 channel w/10 Hz updates - Version 3](https://www.adafruit.com/product/746)
+
 ### The code
-- `SerialDump.ino` is the basis.
+- `SerialDump.ino` shows the basics. 
+- `SerialReader.ino` format the NMEA Sentences, and spit them out in the Serial Console:
+```
+07:50:39.415 -> Setup completed
+07:50:42.417 -> $PGACK,103*40
+07:50:42.417 -> $PGACK,105*46
+07:50:42.453 -> $PMTK011,MTKGPS*08
+07:50:43.424 -> $PMTK010,001*$GPGGA,235942.800,,,,,0,0,,,M,,M,,*4B
+07:50:44.407 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235943.800,,,,,0,0,,,M,,M,,*4A
+07:50:45.428 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235944.799,,,,,0,0,,,M,,M,,*42
+07:50:46.428 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235945.799,,,,,0,0,,,M,,M,,*43
+07:50:47.426 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235946.799,,,,,0,0,,,M,,M,,*40
+07:50:48.413 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235947.799,,,,,0,0,,,M,,M,,*41
+07:50:49.424 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235948.799,,,,,0,0,,,M,,M,,*4E
+07:50:50.429 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235949.799,,,,,0,0,,,M,,M,,*4F
+07:50:51.440 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235950.800,,,,,0,0,,,M,,M,,*48
+07:50:52.426 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235951.800,,,,,0,0,,,M,,M,,*49
+07:50:53.426 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235952.800,,,,,0,0,,,M,,M,,*4A
+07:50:54.425 -> $GPGSA,A,1,,,,,,,,,,,,,,$GPGGA,235953.800,,,,,0,0,,,M,,M,,*4B
+. . .
+```
--- Arduino/SerialReader/SerialReader.ino
+++ Arduino/SerialReader/SerialReader.ino
@@ -1,14 +1,14 @@
 #include <SoftwareSerial.h>  // See https://www.arduino.cc/en/Reference/softwareSerial
 
 /*
- * GPS Wiring
- * 
- * White on pin 0 (RX <- 0) Receives from device (like GPS)
- * Green on pin 1 (TX -> 1)
- * Red on 5V
- * Black on GND
- * 
- */
+   GPS Wiring
+
+   White on pin 0 (RX <- 0) Receives from device (like GPS)
+   Green on pin 1 (TX -> 1)
+   Red on 5V
+   Black on GND
+
+*/
 
 #define RX_PIN 0    // White
 #define TX_PIN 1    // Green - Not necessary if you do read only.
@@ -16,9 +16,9 @@
 SoftwareSerial gps = SoftwareSerial(RX_PIN, TX_PIN);
 
 void setup() {
-//  pinMode(RX_PIN, INPUT);
-//  pinMode(TX_PIN, OUTPUT);
-  
+  //  pinMode(RX_PIN, INPUT);
+  //  pinMode(TX_PIN, OUTPUT);
+
   gps.begin(9600);
   // gps.listen();
 
@@ -29,17 +29,25 @@ void setup() {
   Serial.println("Setup completed");
 }
 
+String nmeaSentence = "";
+String END_OF_NMEA = "\r\n";
+
 void loop() {
-//  gps.listen();
+  //  gps.listen();
   if (gps.available() > 0) {
     char ch = (char)gps.read();
-    Serial.println(ch);
+    nmeaSentence.concat(ch);
+
+    if (nmeaSentence.endsWith(END_OF_NMEA)) {
+      nmeaSentence.trim();
+      Serial.println(nmeaSentence);
+      nmeaSentence = "";
+    }
+    // Serial.println(ch);
   } else {
     delay(1000);
-    Serial.println("No GPS data");
-    if (gps.isListening()) {
-      Serial.println("\tGPS IS listening");
-    } else {
+    if (!gps.isListening()) {
+      Serial.println("No GPS data");
       Serial.println("\tGPS is NOT listening");
     }
   }
