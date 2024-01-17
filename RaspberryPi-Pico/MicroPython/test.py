f7e13039df21adbf9e61e81bea98b0c8abae21c6
OlivierLD <olivier@lediouris.net>
2024-01-17 18:22:09 +0100

    More stuff

--- RaspberryPi-Pico/MicroPython/test.py
+++ RaspberryPi-Pico/MicroPython/test.py
@@ -5,9 +5,9 @@ nmea_string = '$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W
 now = None
 if (nmea_string.startswith("$GPRMC")):
 	try:
-	    ddmmyy = nmea_string[:-3].split(',')[9]
-	    utc = nmea_string[:-3].split(',')[1]
-	    now = "{}-{}-{} {}:{}:{} UTC".format(ddmmyy[0:2], ddmmyy[2:4], ddmmyy[4:6], utc[0:2], utc[2:4], utc[4:6])
+		ddmmyy = nmea_string[:-3].split(',')[9]
+		utc = nmea_string[:-3].split(',')[1]
+		now = "{}-{}-{} {}:{}:{} UTC".format(ddmmyy[0:2], ddmmyy[2:4], ddmmyy[4:6], utc[0:2], utc[2:4], utc[4:6])
 	except Exception as ex:
 		pass
 print("Drive is full, exiting{}.".format((" at " + now) if now != None else ""))
