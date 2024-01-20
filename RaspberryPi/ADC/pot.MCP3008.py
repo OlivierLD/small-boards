fee91ffab094818d5eb66cca1543e5f852f329ab
OlivierLD <olivier@lediouris.net>
2024-01-20 19:26:46 +0100

    With doc

--- RaspberryPi/ADC/pot.MCP3008.py
+++ RaspberryPi/ADC/pot.MCP3008.py
@@ -4,13 +4,14 @@ import gpiozero
 from gpiozero import MCP3008
 from time import sleep
 
-MCP3008_CHANNEL: int = 0
+MCP3008_CHANNEL: int = 0  # in [0..7]
 # create an object called pot that refers to MCP3008 channel 0
 pot: gpiozero.spi_devices.MCP3008 = MCP3008(MCP3008_CHANNEL)
 # print(f"Pot is a {type(pot)}")
 
-prev_value: float = 0
+prev_value: float = -10
 
+# Notice: pot.value goes from 0 to 1, and NOT from 0 to 1024 !!
 try:
     while True:
         if (abs(prev_value - pot.value) > 0.001):
