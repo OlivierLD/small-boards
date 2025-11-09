0412eefde8180a3cde162767f9ea55a68da440b8
OlivierLD <olivier@lediouris.net>
2025-11-09 15:24:30 +0100

    Added first test.

--- AstrolabeExpeditions/FirstBasicTest/VeryFirstTest/VeryFirstTest.ino
+++ AstrolabeExpeditions/FirstBasicTest/VeryFirstTest/VeryFirstTest.ino
@@ -1,24 +1,24 @@
-int led = 2;
+// int led = 2;
+#define THE_LED 2
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Akeu Coucou ! ");
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
+  pinMode(THE_LED, OUTPUT);
+  digitalWrite(THE_LED, LOW);
 }
 
 void loop() {
   Serial.println("Loop top.");
 
   for (int i=0; i<15; i++) {
-    digitalWrite(led, HIGH);
+    digitalWrite(THE_LED, HIGH);
     delay(200);
-    digitalWrite(led, LOW);
+    digitalWrite(THE_LED, LOW);
     delay(200);
   }
 
   delay(1000);
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly...
 
 }
