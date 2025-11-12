f2d1c00ca2eb788a516fd704659437e7eafe06ef
OlivierLD <olivier@lediouris.net>
2025-11-12 11:36:44 +0100

    Sense HAT, act x++

---
+++ RaspberryPi/SenseHat/SenseHatBlink.03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.set_rotation(90)
+red = (255, 0, 0)
+
+try:
+  while True:
+    sense.show_message("Warning!", text_colour=red)
+except KeyboardInterrupt:
+  sense.clear()  # to clear the LED matrix
+
+print("Exiting SenseHatBlink.03.py")
+print("Bye now!")
