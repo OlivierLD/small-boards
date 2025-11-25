38d63757394703b024854367c8caf6f14ff0c185
OlivierLD <olivier@lediouris.net>
2025-11-25 08:22:03 +0100

    Akeu

--- RaspberryPi/SenseHat/startServer.sh
+++ RaspberryPi/SenseHat/startServer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-nohup python3 SenseHatRESTServer.py --machine-name:$(hostname -I) --port:9999 --verbose:false &
+# nohup python3 SenseHatRESTServer.py --machine-name:$(hostname -I) --port:9999 --verbose:false &
+python3 SenseHatRESTServer.py --machine-name:$(hostname -I) --port:9999 --verbose:false &
 echo $! > SenseHatRESTServer.pid
 echo "SenseHatRESTServer started with PID $(cat SenseHatRESTServer.pid)"
+echo "Try curl -X GET http://$(hostname -I | awk '{print $1}'):9999/sense-hat/oplist | jq"
 
 
